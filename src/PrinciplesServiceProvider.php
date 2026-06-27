aaeb70768e02796e5f7f3a8f97575493a66ffb47
aliabedi1 <ali.abedi9731@yahoo.com>
2026-06-27 19:30:47 +0330

    feat: remove the extra class and add package service provider

---
+++ src/PrinciplesServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AliAbedi\Principles;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class PrinciplesServiceProvider extends PackageServiceProvider
+{
+    public function bootingPackage()
+    {
+        logger('Principles package loaded');
+    }
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name('principles');
+    }
+}
